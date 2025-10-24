45222a222c0513ed5a7039fa31dd44b35b050bc4
Ismael Parra M. <ismaelparramoussaif@gmail.com>
2025-10-24 13:29:19 +0200

    documentation - close #1 close #2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola soy Ismael");
+        System.out.println("Hola soy Ismael"); //Si ese soy yo.
     }
 }
